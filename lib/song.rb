d4c74aae235e083836c627e68387577493fcd49c
Eryk Rachwal <erykrachwal@yahoo.com>
2020-01-13 20:08:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,10 +10,9 @@ class Song
     self.class.all << self
   end 
   
-  def self.create(name)
+  def self.create
     song = self.new 
-    song.name = name 
-    @@all << song 
+    song 
   end 
 
 end
